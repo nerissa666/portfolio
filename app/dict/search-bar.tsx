b178458a56a019b742f8c15d4422165f43ba518d
Jude Gao <judegao@outlook.com>
2025-02-08 09:19:31 -0500

    remove hard reload

--- app/dict/search-bar.tsx
+++ app/dict/search-bar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { DICT_HOME } from "./consts";
+import { useSearchParams } from "next/navigation";
+import Form from "next/form";
 
 export const SearchBar = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
   return (
-    <form action={DICT_HOME} className="mb-4 sm:mb-6">
+    <Form action={DICT_HOME} className="mb-4 sm:mb-6" key={query}>
       <div className="flex gap-2">
         <input
           autoFocus
@@ -12,6 +19,6 @@ export const SearchBar = () => {
           className="flex-1 rounded-lg border border-gray-300 px-4 sm:px-6 py-2 sm:py-3 bg-white text-gray-800 placeholder-gray-500 shadow-sm transition-all duration-200 hover:border-gray-400 focus:border-gray-500 focus:ring-1 focus:ring-gray-500 focus:outline-none font-serif text-base sm:text-lg"
         />
       </div>
-    </form>
+    </Form>
   );
 };
