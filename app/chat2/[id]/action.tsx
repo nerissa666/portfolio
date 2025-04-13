381dbcebf9ef11aacbecd7a16abd9ae5310dcf8c
Jude Gao <jude.gao@vercel.com>
2025-04-13 23:32:17 +0800

    fix spinner

--- app/chat2/[id]/action.tsx
+++ app/chat2/[id]/action.tsx
@@ -231,15 +231,25 @@ export const getInitialMessagesReactNode = async (
 
 const Spinner = () => {
   return (
-    <div
-      style={{
-        animation: "spin 0.5s linear infinite",
-        borderRadius: "50%",
-        height: "1rem",
-        width: "1rem",
-        border: "2px solid #d1d5db",
-        borderTopColor: "#4b5563",
-      }}
-    />
+    <>
+      <style>
+        {`
+          @keyframes spin {
+            from { transform: rotate(0deg); }
+            to { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+      <div
+        style={{
+          animation: "spin 0.5s linear infinite",
+          borderRadius: "50%",
+          height: "1rem",
+          width: "1rem",
+          border: "2px solid #d1d5db",
+          borderTopColor: "#4b5563",
+        }}
+      />
+    </>
   );
 };
